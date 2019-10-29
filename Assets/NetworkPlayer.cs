a8be0e0d5d94e35552938da06ed926a1acef76c3
RHolter95 <richardholter1995@gmail.com>
2019-10-29 14:44:34 -0400

    Initial commit

---
+++ Assets/NetworkPlayer.cs
@@ -0,0 +1,67 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+    public class NetworkPlayer : MonoBehaviourPun, IPunObservable
+    {
+
+        protected Animator animator;
+        protected Vector3 realPosition = Vector3.zero;
+        protected Quaternion realRotation = Quaternion.identity;
+        protected float realSpeed;
+        protected bool jumping;
+        private void Awake()
+        {
+        }
+
+        void Start()
+        {
+            animator = GetComponentInChildren<Animator>();
+            if(animator == null){
+                Debug.Log("Animator Empty");
+            }
+        }
+
+        public void Update()
+        {
+            if (this.photonView.IsMine)
+            {
+                //Do Nothing -- Everything is already enabled
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
+                transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
+                Mathf.Lerp(animator.GetFloat("Speed"), realSpeed, 0.1f);
+            }
+            
+        }
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                stream.SendNext(transform.position);
+                stream.SendNext(transform.rotation);
+                stream.SendNext(animator.GetFloat("Speed"));                
+                stream.SendNext(animator.GetBool("IsGrounded"));
+                stream.SendNext(animator.GetFloat("JoyStickX"));
+                stream.SendNext(animator.GetFloat("JoyStickY"));
+
+
+                
+            }
+            else
+            {
+                //This is someone else' player. we need to get their positions
+                //as of a few milliseconds ago and update or version of that player
+                realPosition = (Vector3)stream.ReceiveNext();
+                realRotation = (Quaternion)stream.ReceiveNext();
+                animator.SetFloat("Speed",(float)stream.ReceiveNext());
+                animator.SetBool("IsGrounded",(bool)stream.ReceiveNext());
+                animator.SetFloat("JoyStickX",(float)stream.ReceiveNext());
+                animator.SetFloat("JoyStickY",(float)stream.ReceiveNext());
+
+
+            }
+        }
+    }
---
+++ Assets/Target.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+
+    public Transform player;
+    public Transform target;
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    void LateUpdate()
+    {
+    camera.transform.LookAt(target);
+    camera.transform.Rotate(0,0,0);
+    }
+}
